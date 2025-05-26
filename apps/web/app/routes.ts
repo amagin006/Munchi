ae3bbdff9effc42fb82ea9de82f9e6a2d8bc350d
Shota Iwamoto <amaginqoo@gmail.com>
2025-05-25 20:33:44 -0700

    Add supabase

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,12 +1,12 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
+  // routing page to determine wether navigate main or login
+  index('./index.tsx'),
   // dashboard - (main page)
-  index("./dashboard/index.tsx"),
-
+  route('dashboard', "./dashboard/index.tsx"),
   // auth - (login page)
   route("login", "./auth/login.tsx"),
-
   // settings 
   route("settings", "./settings/index.tsx"),
 ] satisfies RouteConfig;
